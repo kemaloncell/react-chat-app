e4201dcc1254fb89018bec53df34271e1945b6ce
k84255567 <kemal.oncel@huawei.com>
2023-01-17 15:09:49 +0300

    'test'

---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Register = () => {
+    return(
+        <div className='form-container'>
+            <div className='form-container'></div>
+        </div>
+    )
+}
+
+export default Register
