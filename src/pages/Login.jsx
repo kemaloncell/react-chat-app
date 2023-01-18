9d665a8c36c8c49ef1ed594a843eb62e9d3f71f6
k84255567 <kemal.oncel@huawei.com>
2023-01-18 23:02:12 +0300

    created login and register design of pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import Add from '../img/addAvatar.png'
 
 const Login = () => {
     return(
-        <div>Login</div>
+        <div className='formContainer'>
+            <div className='formWrapper'>
+                <span className="logo">Chat App</span>
+                <span className="title">Login</span>
+                <form>
+                    <input type='email' placeholder="email"/>
+                    <input type='password' placeholder="password"/>
+                    <button>Sign In</button>
+                </form>
+                <p>Don't you have an account? Register</p>
+            </div>
+        </div>
     )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Add from '../img/addAvatar.png'
 
 const Register = () => {
     return(
-        <div className='form-container'>
-            <div className='form-container'></div>
+        <div className='formContainer'>
+            <div className='formWrapper'>
+                <span className="logo">Chat App</span>
+                <span className="title">Register</span>
+                <form>
+                    <input type='text' placeholder="display name" />
+                    <input type='email' placeholder="email"/>
+                    <input type='password' placeholder="password"/>
+                    <input style={{display:"none"}} type='file' id="file" />
+                    <label htmlFor="file">
+                        <img src={Add} alt="" className="addImg" />
+                        <span>Add an avatar</span>
+                    </label>
+                    <button>Sign Up</button>
+                </form>
+                <p>Do you have an account? Login</p>
+            </div>
         </div>
     )
 }
