daa76d12c02468b89edc3d840840359954481e60
k84255567 <kemal.oncel@huawei.com>
2023-02-08 21:22:19 +0300

    created sidebar design

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <div className="navbar">
+            <span className="logo">Chat app</span>
+            <div className="user">
+                <img src=""  alt=""/>
+                <span className="">Jhon</span>
+                <button>logout</button>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
